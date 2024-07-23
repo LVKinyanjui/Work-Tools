cad5f609091af28155bd58948efa9eee9914a835
LVKinyanjui <leslievictorius@gmail.com>
2024-07-23 19:36:24 +0300

    Introduced flexible search to help better match locations

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import json
+import json, re
 import pandas as pd
 
 
@@ -17,7 +17,30 @@ df = pd.DataFrame(data)
 st.text_input('Enter a city name', key='city')
 city_name = st.session_state.city
 
-filtered_df = df[df.location.str.contains(city_name)]
+# NAIVE SEARCH
+# filtered_df = df[df.location.str.contains(city_name)]
+
+# FLEXIBLE SEARCH
+def flexible_search(df, column, search_term):
+    """
+    Filter DataFrame based on a flexible search of a specified column.
+    
+    :param df: pandas DataFrame
+    :param column: name of the column to search
+    :param search_term: term to search for
+    :return: filtered DataFrame
+    """
+    # Create a flexible pattern from the search term
+    pattern = re.sub(r'\s+', '.*', re.escape(search_term.strip()))
+    
+    # Create a case-insensitive regular expression
+    regex = re.compile(pattern, re.IGNORECASE)
+    
+    # Filter the DataFrame
+    return df[df[column].str.contains(regex, na=False)]
+
+filtered_df = flexible_search(df, 'location', city_name)
+
 filtered_df.drop(columns=['page_number'], inplace=True)
 
 st.title(f'Work in {city_name}')
